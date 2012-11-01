25a9af47f96951dc968b367c859f64b8e2e9ba14
Paul Belanger <paul.belanger@polybeacon.com>
2012-11-01 15:38:35 -0400

    Add jenkins_job_builder::function::template
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

---
+++ manifests/function/template.pp
@@ -0,0 +1,29 @@
+# == Define: jenkins_job_builder::function::template
+#
+# === Parameters
+#
+# [*content*]
+#
+# === Authors
+#
+# Paul Belanger <paul.belanger@polybeacon.com>
+#
+# === Copyright
+#
+# Copyright (C) 2012, PolyBeacon, Inc.
+#
+# This program is free software, distributed under the terms
+# of the Apache License, Version 2.0. See the LICENSE file at
+# the top of the source tree.
+#
+define jenkins_job_builder::function::template(
+  $content,
+) {
+  file { "${jenkins_job_builder::params::configdir}/${name}":
+    ensure  => file,
+    content => $content,
+    require => Class[$jenkins_job_builder::params::configdir],
+  }
+}
+
+# vim:sw=2:ts=2:expandtab:textwidth=79
